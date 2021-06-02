87cca095ecee0ee9469d115b98cad6bacd617739
Garnier <corentingarnier06@gmail.com>
2021-06-03 01:09:57 +0200

    save des datas des relatives dans les 2 collections

--- src/routes/api/garmin.js
+++ src/routes/api/garmin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Relative = require('../../model/Relative');
+const User = require('../../model/User');
 const router = express.Router();
 
 /**
@@ -49,12 +50,28 @@ router.post('/Sleep', (req, res) => {
  * @todo 
  */
 router.post('/Stress', async(req, res) => {
-    const id = "60b781674e597a1b8432ade0";
+    const id = "60b7817560e599001523b0ce"; //des datas pour karim
     let stressData = req.body;
     await Relative.findOne({ "_id": id }).then(rel => {
         rel.stress.push(stressData);
         rel.save();
     });
+
+    try {
+        const cursor = await User.find();
+
+        cursor.forEach((user) => {
+            user.relatives.forEach((rel) => {
+                if (rel._id == id) {
+                    rel.stress.push(stressData);
+                }
+            });
+            user.save();
+        });
+    } catch (error) {
+        console.error(error)
+    }
+
     return res.status(200).json({
         succes: true,
         msg: "Thanks for your datas salpu !"
